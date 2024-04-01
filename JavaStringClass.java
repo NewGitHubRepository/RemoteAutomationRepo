fcbe180bb893e622ff7392f604d684914f640c50
NewGitHubRepository <geetanjalib0394@gmail.com>
2024-04-01 18:50:35 +0530

    This is Sampled files for testing

---
+++ JavaStringClass.java
@@ -0,0 +1,10 @@
+package org.samplejavapackage.com;
+
+public class JavaStringClass {
+
+	public static void main(String[] args) {
+		// datatype strings concept
+
+	}
+
+}
---
+++ SampleJavaClass.java
@@ -0,0 +1,12 @@
+package org.samplejavapackage.com;
+
+public class SampleJavaClass {
+
+	public static void main(String[] args) {
+		 int a=10; 
+		 int b=20;
+		 System.out.println("Sum of a & b :  "+ (a+b));
+
+	}
+
+}
